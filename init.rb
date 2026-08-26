4ed21d5ce9bc5c75a1ef6b9d3e123c8df3f21a55
Sven Culley <sven@sense-design.de>
2026-08-26 11:02:06 +0200

    Bumped version string

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_project_custom_field_defaults do
   name 'Project Custom Field Defaults'
   author 'Sven Culley'
   description 'Per project default values for issue custom fields, inherited along the project tree and freely overridable on the issue.'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/sense-design/redmine_project_custom_field_defaults'
   author_url 'https://www.linkedin.com/in/sven-culley'
   requires_redmine version_or_higher: '6.0.0'
